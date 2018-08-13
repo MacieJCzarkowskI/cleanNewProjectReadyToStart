9401fbfb6ff3ceb0a4c40cbf9d85526d7843a36c
maciej.czarkowski <maciej.czarkowski@sagiton.pl>
2018-08-13 22:38:34 +0200

    patten for email and password (oAuth working)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,9 +12,10 @@ export class LoginComponent implements OnInit {
   public loginText: string;
   public errorMessage = '';
   public ifError = false;
-  public activeLogin = false;
-  public emailValid;
-  public isSubmitted;
+  public isWrongLoginPattern = true;
+  public isWrongPasswordPatten = true;
+  public loginErrorPattern = false;
+  public passwordErrorPattern = false;
 
   public wrongLogin = false;
   public loginPlaceholder = 'login@example.com';
@@ -26,14 +27,35 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   login() {
+    const emailPattern = new RegExp(/^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/);
+    this.isWrongLoginPattern = emailPattern.test(this.loginText);
+    const passwordPattern = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/);
+    this.isWrongPasswordPatten = passwordPattern.test(this.passwdText);
+    if (!this.isWrongLoginPattern && !this.isWrongLoginPattern) {
+      this.loginErrorPattern = true;
+      this.passwordErrorPattern = true;
+      return 0;
+    }
+    if (!this.isWrongPasswordPatten && this.isWrongLoginPattern) {
+      this.loginErrorPattern = false;
+      this.passwordErrorPattern = true;
+      return 0;
+    }
+    if (!this.isWrongLoginPattern && this.isWrongPasswordPatten) {
+      this.passwordErrorPattern = false;
+      this.loginErrorPattern = true;
+      return 0;
+    }
+    if (this.isWrongLoginPattern && this.isWrongPasswordPatten) {
+      this.loginErrorPattern = false;
+      this.passwordErrorPattern = false;
+    }
     this.authenticationService.loginToProject(this.loginText, this.passwdText).then( (success: any) => {
-      console.log('success', success);
       localStorage.setItem('token', success.access_token)
       this.ifError = false;
       this.router.navigateByUrl('/dashboard');
       alert(success.message);
     }, error => {
-      console.log('error', error);
       this.errorMessage = error.error.message;
       this.ifError = true;
     });
